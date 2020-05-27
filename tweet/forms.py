f5122a47c1e8d37f73f8a9e40f9a64561ed2081c
Mike A <jimalik2525@gmail.com>
2020-05-27 16:59:42 -0400

    Added tweetform

--- tweet/forms.py
+++ tweet/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class AddTweetForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea, max_length=140)
+    text = forms.CharField(widget=forms.Textarea, max_length=140,
+                           help_text="Limit: 140 characters max")
