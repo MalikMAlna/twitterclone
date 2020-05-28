8ead6c73fd12f706cd45b0d68a98ccf973ca55da
Mike A <jimalik2525@gmail.com>
2020-05-28 08:09:40 -0400

    Working on notifications

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from twitteruser.models import TwitterUser
+from tweet.models import Tweet
 
-# Create your models here.
+
+class Notification(models.Model):
+    tweet = models.ForeignKey(Tweet)
+    tweet_author = models.ForeignKey(TwitterUser)
